abf2df46f45aef278e80a4aeb445f0a8918a0df7
Dwi Ayuni Rohana <89717584+DwiAyuniRohana@users.noreply.github.com>
2021-09-01 23:03:22 +0700

    Create admin.php

---
+++ admin.php
@@ -0,0 +1,68 @@
+<?php 
+session_start();
+if (!$_SESSION['berhasil']){
+	header("Location: login.php");
+	die();
+} 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+<link rel="stylesheet" href="css/desain.css">
+    <title>Galeriku</title>
+</head>
+<body>
+<!-- awal tampilan -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-link" href="admin.php">HOME <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="login.php">LOG OUT</a>
+    </div>
+  </div>
+  </div>
+</nav>
+
+<div class="container">
+    <div class="row">
+        <div class="col text-center">
+<div class="col text-center">
+ <h1>Dwi Ayuni's Gallery</h1>
+</div>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active" >
+      <img class="d-block w-100" src="img/bali 2.jpg" opacity="-7" alt="First slide" >
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="img/bali 6.jpg" opacity= "-7" alt="Second slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
